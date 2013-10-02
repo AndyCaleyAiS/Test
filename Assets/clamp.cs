66bf12a95114edb8addaf9b166e294fa9a613267
Andy Caley <acaley@gmail.com>
2013-10-01 22:04:27 -0700

    First Commit!
    some stuff!

---
+++ Assets/clamp.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clamp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			this.transform.Translate(Vector3.up * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			this.transform.Translate(Vector3.down * Time.deltaTime);
+		}
+	
+		Vector3 pos = this.transform.position;
+		pos.y = Mathf.Clamp(pos.y, -1, 1);
+		this.transform.position = pos;
+	}
+}
---
+++ Assets/collider.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collider : MonoBehaviour {
+	
+	public int speed = 3;
+	
+	// Use this for initialization
+	void Start () {
+	
+		Vector3 pos = new Vector3(1,3,5);
+		Vector3 pos2 = new Vector3(2,6,1);
+		
+		Vector3 output = pos * 4;
+		
+		float angle = Vector3.Angle(pos2 - pos, Vector3.right);
+		Debug.Log(angle);
+		
+		Debug.Log (Vector3.ClampMagnitude(new Vector3(2,1,4), 2));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+	}
+	
+	void OnTriggerEnter(Collider collider)
+	{
+		Debug.Log ("Collided");
+		speed = -speed;		
+		
+		float angle = Vector3.Angle(this.transform.forward,  collider.transform.position - this.transform.position);
+		Debug.Log ("Angle: " + angle);
+		Debug.Log ("Distance: " + Vector3.Distance(this.transform.position, collider.transform.position));
+	}
+}
